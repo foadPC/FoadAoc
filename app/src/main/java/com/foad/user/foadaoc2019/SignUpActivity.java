d97d92f7bf7e4654b2b8fc6db770d2990ba3e271
foadPC <foad.asmer@gmail.com>
2019-09-19 14:28:43 +0300

    sign up page and login

--- app/src/main/java/com/foad/user/foadaoc2019/SignUpActivity.java
+++ app/src/main/java/com/foad/user/foadaoc2019/SignUpActivity.java
@@ -1,4 +1,13 @@
 package com.foad.user.foadaoc2019;
 
-class SignUpActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
 }
