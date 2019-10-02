ce10ced4894e051612f46f3381a769f56a78a927
foadGithub123! <foad.asmer@gmail.com>
2019-10-02 11:11:04 +0300

    fix

--- app/src/main/java/com/foad/user/foadaoc2019/SignUpActivity.java
+++ app/src/main/java/com/foad/user/foadaoc2019/SignUpActivity.java
@@ -1,13 +1,31 @@
 package com.foad.user.foadaoc2019;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
+    Button confirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        confirm = findViewById(R.id.confirm);
+        confirm.setOnClickListener(this);
     }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v==confirm){
+            Intent i = new Intent(this, LogInActivity.class);
+            startActivity(i);
+        }
+    }
+
+
 }
