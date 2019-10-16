7c9df436e4b30ec6a71dd92e83dae241ebf5c3cc
foadGithub123! <foad.asmer@gmail.com>
2019-10-16 09:59:42 +0300

    PUSHIN

--- app/src/main/java/com/foad/user/foadaoc2019/SignUpActivity.java
+++ app/src/main/java/com/foad/user/foadaoc2019/SignUpActivity.java
@@ -5,14 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText editTextFirstName,editTextLastName,editTextEmailSign,editTextPasswordSign,editTextConfirmPassword;
     Button confirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        editTextFirstName = findViewById(R.id.editTextFirstName);
+        editTextLastName = findViewById(R.id.editTextLastName);
+        editTextEmailSign = findViewById(R.id.editTextEmailSign);
+        editTextPasswordSign = findViewById(R.id.editTextPasswordSign);
+        editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword);
+
 
         confirm = findViewById(R.id.confirm);
         confirm.setOnClickListener(this);
@@ -22,8 +32,17 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     public void onClick(View v) {
         if(v==confirm){
-            Intent i = new Intent(this, LogInActivity.class);
-            startActivity(i);
+            if (editTextFirstName.getText().toString().equals("")
+                    || editTextLastName.getText().toString().equals("")
+                     || editTextEmailSign.getText().toString().equals("")
+                      || editTextPasswordSign.getText().toString().equals("")
+                       || editTextConfirmPassword.getText().toString().equals(""))
+            {
+                Toast.makeText(this, "A Field Is Empty", Toast.LENGTH_LONG).show();
+            }
+           if((!editTextConfirmPassword.getText().toString().equals(editTextPasswordSign))){
+               Toast.makeText(this, "Passwords Dont Match", Toast.LENGTH_LONG).show();
+           }
         }
     }
 
