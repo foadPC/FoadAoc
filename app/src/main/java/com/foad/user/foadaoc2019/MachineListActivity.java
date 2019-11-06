fc473dba64e6c6002bcbc376538c21fbad6390cd
foadGithub123! <foad.asmer@gmail.com>
2019-11-06 11:07:10 +0200

    complete later

--- app/src/main/java/com/foad/user/foadaoc2019/MachineListActivity.java
+++ app/src/main/java/com/foad/user/foadaoc2019/MachineListActivity.java
@@ -5,16 +5,19 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MachineListActivity extends AppCompatActivity {
 
     ListView machineList;
     Button addMachineButton;
+    ArrayList<Machine> machines = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_machine_list);
 
-
+        machines.add(new Machine());
     }
 }
