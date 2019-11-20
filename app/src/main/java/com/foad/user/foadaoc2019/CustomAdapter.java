c7fc7c2c4a62951e0f9d8124ea3b921b6a34085f
foadGithub123! <foad.asmer@gmail.com>
2019-11-20 12:01:26 +0200

    custom adapter start

---
+++ app/src/main/java/com/foad/user/foadaoc2019/CustomAdapter.java
@@ -0,0 +1,13 @@
+package com.foad.user.foadaoc2019;
+
+import android.content.Context;
+import
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+public class CustomAdapter extends ArrayAdapter<Machine> {
+    private int resourceLayout;
+    private Context mContext;
+
+
+}
--- app/src/main/java/com/foad/user/foadaoc2019/MachineListActivity.java
+++ app/src/main/java/com/foad/user/foadaoc2019/MachineListActivity.java
@@ -2,22 +2,32 @@ package com.foad.user.foadaoc2019;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MachineListActivity extends AppCompatActivity {
 
     ListView machineList;
     Button addMachineButton;
     ArrayList<Machine> machines = new ArrayList<>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_machine_list);
 
-        machines.add(new Machine());
+        machineList = findViewById(R.id.machineList);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, machines);
+
+        machines.add(new Machine("machine 1",new Date(), new Date(), new Date()) );
+        machines.add(new Machine("machine 2",new Date(), new Date(), new Date()) );
+        machines.add(new Machine("machine 3",new Date(), new Date(), new Date()) );
+
+
     }
 }
