267a76e0da14fe13a7c0d51d7d6038bc34de8c7e
foadGithub123! <foad.asmer@gmail.com>
2019-11-21 13:03:09 +0200

    custom adapter start

--- app/src/main/java/com/foad/user/foadaoc2019/CustomAdapter.java
+++ app/src/main/java/com/foad/user/foadaoc2019/CustomAdapter.java
@@ -1,13 +1,48 @@
 package com.foad.user.foadaoc2019;
 
 import android.content.Context;
-import
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import java.util.List;
+
 public class CustomAdapter extends ArrayAdapter<Machine> {
     private int resourceLayout;
     private Context mContext;
+
+    public CustomAdapter(Context context, int resource, List<Machine> items) {
+        super(context, resource, items);
+        this.resourceLayout = resource;
+        this.mContext = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View v = convertView;
+        if(v == null)
+            v = LayoutInflater.from(mContext).inflate(resourceLayout,parent,false);
+
+
+        Machine p = getItem(position);
+
+        if (p != null) {
+            TextView tvName = (TextView) v.findViewById(R.id.);
+            tvName.setText(p.getIngredient());
+
+            CheckBox checkBox = v.findViewById(R.id.checkBox);
+            checkBox.setChecked(p.isCheckbox());
+
+        }
+
+        return v;
+    }
+
+}
 
 
 }
