88e2621185a38cb224dab8544fe2550296c3855a
foadGithub123! <foad.asmer@gmail.com>
2020-02-26 11:18:50 +0200

    specific machine details page still not done

--- app/src/main/java/com/foad/user/foadaoc2019/MachineListActivity.java
+++ app/src/main/java/com/foad/user/foadaoc2019/MachineListActivity.java
@@ -2,6 +2,7 @@ package com.foad.user.foadaoc2019;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -9,12 +10,13 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class MachineListActivity extends AppCompatActivity {
+public class MachineListActivity extends AppCompatActivity implements View.OnClickListener {
 
     ListView machineList;
     Button addMachineButton;
     ArrayList<Machine> machines = new ArrayList<>();
     CustomAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +24,19 @@ public class MachineListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_machine_list);
 
         machineList = findViewById(R.id.machineList);
-        adapter = new CustomAdapter(this, R.layout.machine_item, machines);
-
         machines.add(new Machine("machine 1",new Date(), new Date(), new Date()) );
         machines.add(new Machine("machine 2",new Date(), new Date(), new Date()) );
         machines.add(new Machine("machine 3",new Date(), new Date(), new Date()) );
+        adapter = new CustomAdapter(this, R.layout.machine_item, machines);
+        machineList.setAdapter(adapter);
 
+        addMachineButton = findViewById(R.id.addMachineButton);
+        addMachineButton.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        make an intent lsf7t details 3n lmachines :)
     }
 }
--- app/src/main/java/com/foad/user/foadaoc2019/MachinesActivity.java
+++ app/src/main/java/com/foad/user/foadaoc2019/MachinesActivity.java
@@ -1,13 +1,36 @@
 package com.foad.user.foadaoc2019;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MachinesActivity extends AppCompatActivity {
+public class MachinesActivity extends AppCompatActivity implements View.OnClickListener{
+    Button respButton,sleepButton;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_machines_activty);
+
+         sleepButton= findViewById(R.id.sleepButton);
+         sleepButton.setOnClickListener(this);
+
+        respButton = findViewById(R.id.respButton);
+        respButton.setOnClickListener(this);
+    }
+    public void onClick(View v){
+        if (v == respButton) {
+            Intent i = new Intent(this, MachineListActivity.class);
+            startActivity(i);
+        }
+        if (v == sleepButton){
+            Intent i = new Intent(this, MachineListActivity.class);
+            startActivity(i);
+        }
     }
 }
